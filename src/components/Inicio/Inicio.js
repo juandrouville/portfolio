c60ae495c2f2ff58482fc54b9f0d39e85bd945f7
juandrouville <juandrouville>
2022-05-23 21:29:06 -0300

    little changes

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -3,12 +3,12 @@ import './Inicio.css';
 import img from '../../portfolio-bodoni-white.svg';
 
 
+
+
 function Inicio(){
     return (
         <div className="inicio">
             <img src={img}/>
-            <div className="violeta"></div>
-            <div className="rosa"></div>
         </div>
             
     )
